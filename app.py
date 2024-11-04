1f3b99e73c76e406a087421d8c2fb055226ff335
karstenkunneman <karstenkunneman@mines.edu>
2024-11-04 11:33:30 -0700

    Update 11/4/2024

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import streamlit as st
 import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
+import time
 
-componentList = ['1. Methane', '2. Ethane', '3. Propane', '4. Isobutane', '5. Cyclopropane', '6. Hydrogen Sulfide', '7. Nitrogen', 'Carbon Dioxide']
+componentList = ['1. Methane', '2. Ethane', '3. Propane', '4. Isobutane', '5. Cyclopropane', '6. Hydrogen Sulfide', '7. Nitrogen', '8. Carbon Dioxide']
 
 st.title('Gas Hydrate Equilibrium Calculator')
-st.caption('Version 2024-4-15')
+st.caption('Version 2024-10-31')
 
 noComponents = st.number_input('Number of Components', 1, None, 1, 1)
 components = []
@@ -20,23 +21,44 @@ with c2:
     for i in range(noComponents):
         moleFractions += [float(st.text_input('Component ' + str(i+1) + " Mole Fraction", 1.00))]
 
-guessPressure = float(st.text_input('Guess Pressure (MPA)', 1.00))*1E6
+csvGuesses = st.file_uploader("Upload Temperatures and Guess Pressures", ['csv'])
+csvTemplate = st.download_button("Guess File Template", open("Input Template.csv", encoding='utf-8'), file_name="Input Template.csv")
 
-calculateRange = st.toggle('Calculate Range of Temperatures', False)
+if csvGuesses == None:
+    calculateRange = st.toggle('Calculate Range of Temperatures', False)
+    if calculateRange == True:
+        minTemp = float(st.text_input('Minimum Temperature (K)', 148.8))
+        maxTemp = float(st.text_input('Maximum Temperature (K)', 270.9))
+        noPoints = st.number_input('Number of Points', 1, None, 8, 1)
+        T = numpy.arange(minTemp, maxTemp+(maxTemp-minTemp)/noPoints, (maxTemp-minTemp)/(noPoints-1))
 
-if calculateRange == True:
-    minTemp = float(st.text_input('Minimum Temperature (K)', 275))
-    maxTemp = float(st.text_input('Maximum Temperature (K)', 300))
-    noPoints = st.number_input('Number of Points', 1, None, 8, 1)
-    T = numpy.arange(minTemp, maxTemp+(maxTemp-minTemp)/noPoints, (maxTemp-minTemp)/(noPoints-1))
+        minGuessPressure = float(st.text_input("Minimum Guess Pressure (MPa): ", 0.056))*1E6 #Pressure in Pa
+        maxGuessPressure = float(st.text_input("Maximum Guess Pressure (MPa): ", 2.39))*1E6 #Pressure in Pa
+        P = numpy.arange(maxGuessPressure, minGuessPressure-(maxGuessPressure-minGuessPressure)/noPoints, -1*(maxGuessPressure-minGuessPressure)/(noPoints-1))
+    else:
+        T = [float(st.text_input('Temperature (K)', 298))]
+        P = [float(st.text_input('Guess Pressure (MPA)', 1.00))*1E6]
 else:
-    T = [float(st.text_input('Temperature (K)', 298))]
+    guessFile = numpy.genfromtxt(csvGuesses, delimiter=',', skip_header=1)
+    T = guessFile[:,0]
+    P = guessFile[:,1]*1E6
+    noPoints = len(T)
+    if noPoints > 1:
+        calculateRange = True
+    else:
+        calculateRange = False
     
 if st.button("Calculate"):
+    startTime = time.time()
     if calculateRange == True:
         eqPressure = [0 for i in range(len(T))]
+        eqStructure = [0 for i in range(len(T))]
+        eqFractions = [0 for i in range(len(T))]
         for i in range(len(T)):
-            eqPressure[i] = simulation.equilibriumPressure(T[i], guessPressure, components, moleFractions)/1E6 #In MPa
+            simResult = simulation.equilibriumPressure(T[i], P[i], components, moleFractions)
+            eqPressure[i] = simResult[0]/1E6 #In MPa
+            eqStructure[i] = simResult[1]
+            eqFractions[i] = simResult[2]
         fig = plt.figure()
         plt.plot(T, eqPressure, '-ok')
         plt.yscale("log")
@@ -45,15 +67,21 @@ if st.button("Calculate"):
         plt.ylabel("Pressure (MPa)")
         st.pyplot(fig)
     else:
-        eqPressure = [simulation.equilibriumPressure(T[0], guessPressure, components, moleFractions)/1E6]
-    data = pd.DataFrame({'Temp (K)': T, 'Eq. Pressure (MPa)': eqPressure}, [i for i in range(len(T))])
+        simResult = simulation.equilibriumPressure(T[i], P[i], components, moleFractions)
+        eqPressure = simResult[0]/1E6 #In MPa
+        eqStructure = simResult[1]
+        eqFractions = simResult[2]
+    data = pd.DataFrame({'Temp (K)': T, 'Eq. Pressure (MPa)': eqPressure, 'Eq. Structure': eqStructure, 'Occupancy Fractions': eqFractions}, [i for i in range(len(T))])
     st.dataframe(data, hide_index = True)
+    endTime = time.time()
+    st.text("Time to Complete Calculation: " + str(round(endTime-startTime, 3)) + " seconds")
+    st.text("per Data Point: " + str(round((endTime-startTime)/noPoints, 3)) + " seconds")    
 
 st.header('Credits')
 st.markdown('''
             Created by Karsten Kunneman and Dr. Amadeu K Sum at the Colorado School of Mines  
             Based on "A Fugacity Model for Gas Hydrate Phase Equilibria" by Jeffery B. Klauda and Stanley I. Sandler  
-            Compound data obrained from NIST  
+            Compound data obtained from NIST  
             Equations used from "PRSV: An Improved Peng- Robinson Equation of State for Pure Compounds and Mixtures" by R. Stryjek and J. H. Vera  
             Binary interaction parameters obtained from "Vapor-liquid equilibria for mixtures of low boiling substances. Pt. 2. Ternary systems" by H. Knapp, S. Zeck, and R. Langhorst  
             This site created with Streamlit
--- simulation.py
+++ simulation.py
@@ -1,9 +1,15 @@
 import math 
-import matplotlib.pyplot as plt
 import numpy
+import matplotlib.pyplot as plt
 import pandas
 import scipy
+from thermo.unifac import UNIFAC, PSRKSG, PSRKIP
+import ast
+import time
+
+#REMOVE EVENTUALLY
 import warnings
+warnings.filterwarnings("ignore")
 
 #Extract data from excel files
 fluidProperties = pandas.read_excel('Data.xlsx', sheet_name='Fluid Properties')
@@ -13,7 +19,7 @@ vaporPressureConstants = pandas.read_excel('Data.xlsx', sheet_name='Vapor Pressu
 mixConstants = pandas.read_excel('Data.xlsx', sheet_name='Binary Interaction Parameters')
 
 #Constants
-errorMargin = 1E-4
+errorMargin = 1E-9
 R = 8.31446261815324 #m^3 Pa/mol K
 boltzmannConstant = 1.380649E-23 #m^2 kg/s^2 K
 
@@ -36,7 +42,7 @@ def PengRobinson(compoundData, moleFractions, T, P):
         omega = compoundData[i][4]
         kappa1 = compoundData[i][5]
         kappa = 0.378893 + 1.4897153*omega - 0.17131848*omega**2 + 0.0196554*omega**3 + kappa1*(1+math.sqrt(T/Tc))*(0.7-(T/Tc))
-        kappa = 0.37464 + 1.54226*omega - 0.26992*(omega**2)
+        #kappa = 0.37464 + 1.54226*omega - 0.26992*(omega**2)
         alpha = (1 + kappa*(1-math.sqrt(T/Tc)))**2
         a[i] = 0.45724*((R**2)*(Tc**2)/Pc)*alpha
         
@@ -86,19 +92,19 @@ def PengRobinson(compoundData, moleFractions, T, P):
         fugVap[i] = P*moleFractions[i]*math.exp((b[i]/bmix)*(ZVmix-1)-math.log(ZVmix-B)-(A/(2*math.sqrt(2)*B))*(2*xia[i]/amix-b[i]/bmix)*math.log((ZVmix+(1+math.sqrt(2))*B)/(ZVmix+(1-math.sqrt(2))*B)))
         #fugLiq[i] = P*moleFractions[i]*math.exp((b[i]/bmix)*(ZLmix-1)-math.log(ZLmix-B)-(A/(2*math.sqrt(2)*B))*(2*xia[i]/amix-b[i]/bmix)*math.log((ZLmix+(1+math.sqrt(2))*B)/(ZLmix+(1-math.sqrt(2))*B)))
         
-    return VmVap, VmLiq, fugVap#, fugLiq
+    return VmVap, VmLiq, fugVap, ZVmix
 
 #For a given hydrate structure, return its radii and coordination numbers (DONE)
 def getHydrateCellProperties(structure):
     cellProperties = numpy.array(hydrateCellProperties.loc[(hydrateCellProperties['Structure'] == structure)])
     return cellProperties
    
-#Equation 7 (DONE)
+#Equation 7
 def delta(N, r, RCell, a):
     delta = ((1-r/RCell-a/RCell)**(-1*N)-(1+r/RCell-a/RCell)**(-1*N))/N
     return delta 
    
-#Equation 6 
+#Equation 6
 def W(r, RCell, z, epsilon, sigma, a):
     d10 = delta(10, r, RCell, a)
     d11 = delta(11, r, RCell, a)
@@ -107,21 +113,21 @@ def W(r, RCell, z, epsilon, sigma, a):
     W = 2*z*epsilon*((sigma**12)/(r*RCell**11)*(d10+a/RCell*d11)-sigma**6/(r*RCell**5)*(d4+a/RCell*d5))
     return W  
    
-#Equation 4 
+#Equation 4
 def Lang_Const(T, cellRadii, a, RCell, z, epsilon, sigma):
     def integrand(r):
         W1 = W(r, cellRadii[0], z[0], epsilon, sigma, a)
-        W2 = W(r, cellRadii[1], z[1], epsilon, sigma, a)
-        W3 = W(r, cellRadii[2], z[2], epsilon, sigma, a)
+        W2 = W(1E-14, cellRadii[1], z[1], epsilon, sigma, a)
+        W3 = W(1E-14, cellRadii[2], z[2], epsilon, sigma, a)
         x = math.exp(-1*(W1+W2+W3)/T)*(r**2)
         return x
     
     Cml = 4*math.pi/(boltzmannConstant*T)*(scipy.integrate.quad(integrand, 0, RCell-a)[0])
     return Cml
 
-#Equation 3 (DONE)
+#Equation 3
 def frac(T, kiharaParameters, cellProperties, vaporFugacities):
-    fractions = [[0 for i in range(len(vaporFugacities))], [0 for i in range(len(vaporFugacities))]]
+    fractions = [[0 for i in range(len(vaporFugacities))],[0 for i in range(len(vaporFugacities))]]
     for i in range(2):
         denominator = 0
         #Hydrate Cell Properties
@@ -147,66 +153,98 @@ def frac(T, kiharaParameters, cellProperties, vaporFugacities):
     
     return fractions
 
-#Equation 2 (DONE)
+#Equation 2
 def deltaHydratePotential(T, kiharaParameters, structure, vaporFugacityCoeffs):
-    cellProperties = getHydrateCellProperties(structure)
+    cellProperties = getHydrateCellProperties(structure) 
     fractions = 0
     Deltamu_H_w = 0
     
     fractions = frac(T, kiharaParameters, cellProperties, vaporFugacityCoeffs)
-    Deltamu_H_w += -1*R*T*(cellProperties[0][10])*math.log(1-sum(fractions[0]))
-    Deltamu_H_w += -1*R*T*(cellProperties[1][10])*math.log(1-sum(fractions[1]))
-    return Deltamu_H_w
+    
+    Deltamu_H_w += cellProperties[0][10]*math.log(1-sum(fractions[0]))
+    Deltamu_H_w += cellProperties[1][10]*math.log(1-sum(fractions[1]))
+    return Deltamu_H_w, fractions
 
-#Equation 18 (DONE)
+#Equation 18
 def henrysLawConst(compound, T):
     henrysLawConstants = pandas.read_excel('Data.xlsx', sheet_name='Henrys Law Parameters')
     constants = numpy.array(henrysLawConstants.loc[henrysLawConstants['Compound ID'] == compound])[0]
     H_i = 101325*math.exp(-1*(constants[2] + constants[3]/T + constants[4]*math.log(T) + constants[5]*T))
     return H_i
 
-#Equation 17 (DONE)
-def liqPhaseComposition(compounds, T, fug_vap, Vi, P, Psat):
+#Infinite Dilution Compressibility Factor
+def Z(compoundData, T, P):
+    Z = [0 for i in range(len(compoundData))]
+    waterData = numpy.array(fluidProperties.loc[fluidProperties['Compound ID'] == 0])[0]
+    for i in range(len(compoundData)):
+        localCompoundData = compoundData[i]
+        localCompoundData = numpy.column_stack((localCompoundData, waterData))
+        localCompoundData = localCompoundData.transpose()
+        Z[i] = PengRobinson(localCompoundData, [0.00001, 0.99999], T, P)[3]
+    return Z
+
+#Equation 17
+def liqPhaseComposition(compounds, T, fug_vap, compoundData, P, Psat):
     x = [0 for i in range(len(compounds)+1)]
     for i in range(len(fug_vap)):
-        x[i+1] = fug_vap[i]/(henrysLawConst(compounds[i], T)*math.exp(Vi[i]*(P-Psat)/(R*T)))
+        Z_inf = Z(compoundData, T, P)
+        x[i+1] = fug_vap[i]/(henrysLawConst(compounds[i], T)*math.exp(Z_inf[i]))
     x[0] = 1-sum(x) #Water composition
     return x
 
 #Equation 19
-def freezingPointDepression():
-    deltadT = R*(273.15)**2/6011*math.log(1)
+def freezingPointDepression(compounds, T, fug_vap, compoundData, P, chemGroups):
+    if T > 273.15:
+        Psat = math.exp(4.1539*math.log(T)-5500.9332/T+7.6537-16.1277E-3*T)
+    else:
+        Psat = math.exp(4.6056*math.log(T)-5501.1243/T+2.9446-T*8.1431E-3)
+    
+    phaseComposition = liqPhaseComposition(compounds, T, fug_vap, compoundData, P, Psat)
+    deltadT = R*(273.15)**2/6011*math.log(liqPhaseComposition(compounds, T, fug_vap, compoundData, P, Psat)[0]*activityCoeff(T, phaseComposition, chemGroups))
     return deltadT
 
-def activityCoeff():
-    return 1
+def activityCoeff(T, phaseComposition, chemGroups):
+    chemGroupList = [{16: 1}]
+    for i in range(len(chemGroups)):
+        chemGroupList.append(ast.literal_eval(chemGroups[i]))
 
-#Equations 15 and 16
-def waterFugacity(T, P, phase, fug_vap, compounds, Vi):
+    GE = UNIFAC.from_subgroups(T, phaseComposition, chemGroupList, interaction_data=PSRKIP, subgroups=PSRKSG).gammas()[0]
+    return GE
+    
+#Equations 15 and 16 
+def waterFugacity(T, P, phase, fug_vap, compounds, compoundData):
     if phase == "ice":
         Vm_water = 1.912E-5 + T*8.387E-10 + (T**2)*4.016E-12
         Psat_water = math.exp(4.6056*math.log(T)-5501.1243/T+2.9446-T*8.1431E-3)
         f_w = Psat_water*math.exp(Vm_water*(P-Psat_water)/(R*T))
     elif phase == "liquid":
+        chemGroups = compoundData[:,7]
         Vm_water = math.exp(-10.921 + 2.5E-4*(T-273.15) - 3.532E-4*(P/1E6-0.101325) + 1.559E-7*((P/1E6-.101325)**2))
         Psat_water = math.exp(4.1539*math.log(T)-5500.9332/T+7.6537-16.1277E-3*T)
-        f_w = liqPhaseComposition(compounds, T, fug_vap, Vi, P, Psat_water)[0]*activityCoeff()*Psat_water*math.exp(Vm_water*(P-Psat_water)/(R*T))
+        phaseComposition = liqPhaseComposition(compounds, T, fug_vap, compoundData, P, Psat_water)
+        f_w = phaseComposition[0]*activityCoeff(T, phaseComposition, chemGroups)*Psat_water*math.exp(Vm_water*(P-Psat_water)/(R*T))
     return f_w
 
 #Equation A2
-def hydrateFugacity(T, P, PvapConsts, structure, fug_vap, compounds, kiharaParameters):
+def hydrateFugacity(T, P, PvapConsts, structure, fug_vap, compounds, kiharaParameters, moleFractions):
     N_A = 6.022E23
     if structure == "I":
-        Vm_water = (11.835+2.217E-5*T+2.242E-6*T**2)*(1E-30*N_A/46)-8.006E-9*P/1E6+5.448E-12*(P/1E6)**2
-        #Vm_water = (11.835+2.217E-5*T+2.242E-6*T**2)**3*(1E-30*N_A/46)
+        Vm_water = (11.835+2.217E-5*T+2.242E-6*T**2)**3*(1E-30*N_A/46)-8.006E-9*P/1E6+5.448E-12*(P/1E6)**2
     elif structure == "II":
-        Vm_water = (17.13+2.249E-4*T+2.013E-6*T**2+1.009E-9*T**3)*(1E-30*N_A/136)-8.006E-9*P/1E6+5.448E-12*(P/1E6)**2
-    A = min(PvapConsts[:,3])
-    B = min(PvapConsts[:,4]) #Use min value (maybe fix later)
-    D = min(PvapConsts[:,6])
+        Vm_water = (17.13+2.249E-4*T+2.013E-6*T**2-1.009E-9*T**3)**3*(1E-30*N_A/136)-8.006E-9*P/1E6+5.448E-12*(P/1E6)**2
+    A = 0
+    B = 0
+    D = 0
+    for i in range(len(compounds)):
+        A += PvapConsts[i,3]*moleFractions[i]
+        B += PvapConsts[i,4]*moleFractions[i]
+        D += PvapConsts[i,6]*moleFractions[i]
+    
     Psat_water = math.exp(A*math.log(T)+B/T+2.7789+D*T)
-    f_h = Psat_water*math.exp(Vm_water*(P-Psat_water)/(R*T))*math.exp(-1*deltaHydratePotential(T, kiharaParameters, structure, fug_vap)/(R*T))
-    return f_h
+    dH = deltaHydratePotential(T, kiharaParameters, structure, fug_vap)
+    frac = dH[1]
+    f_h = Psat_water*math.exp(Vm_water*(P-Psat_water)/(R*T))*math.exp(dH[0])
+    return f_h,frac
 
 def equilibriumPressure(temperature, pressure, compounds, moleFractions):
     compoundData = numpy.array(fluidProperties.loc[fluidProperties['Compound ID'] == compounds[0]])
@@ -215,62 +253,98 @@ def equilibriumPressure(temperature, pressure, compounds, moleFractions):
 
     PvapConsts = numpy.array(vaporPressureConstants.loc[vaporPressureConstants['Compound ID'] == compounds[0]])
     for i in range(len(compounds)-1):
-        PvapConsts = numpy.append(vaporPressureConstants, vaporPressureConstants.loc[vaporPressureConstants['Compound ID'] == compounds[i+1]], axis = 0)
+        PvapConsts = numpy.append(PvapConsts, vaporPressureConstants.loc[vaporPressureConstants['Compound ID'] == compounds[i+1]], axis = 0)
 
     kiharaParameters = numpy.array(kiharaCellParameters.loc[kiharaCellParameters['Compound ID'] == compounds[0]])
     for i in range(len(compounds)-1):
         kiharaParameters = numpy.append(kiharaParameters, kiharaCellParameters.loc[kiharaCellParameters['Compound ID'] == compounds[i+1]], axis = 0)
 
     #Computational Algorithm
+    pGuess = pressure
+    if temperature > 260 and temperature < 280:
+        freezingPoint = 273.15+freezingPointDepression(compounds, temperature, PengRobinson(compoundData, moleFractions, 273.15, pressure)[2], compoundData, pGuess, compoundData[:,7])
+        if temperature > freezingPoint:
+            waterPhase = "liquid"
+        else:
+            waterPhase = "ice"
+    elif temperature <= 260:
+        waterPhase = "ice"
+    elif temperature >= 280:
+        waterPhase = "liquid"
+    
+    def f(pressure):
+        vaporFugacities = PengRobinson(compoundData, moleFractions, temperature, pressure)[2]
+        f_w = waterFugacity(temperature, pressure, waterPhase, vaporFugacities, compounds, compoundData)
+        f_h = hydrateFugacity(temperature, pressure, localPvapConsts, structure, vaporFugacities, compounds, kiharaParameters, moleFractions)[0]
+        return abs(f_h/f_w-1)
+        
     structure = "I"
     pGuess = pressure
-
-    if temperature > 273.15-freezingPointDepression():
-        waterPhase = "liquid"
-    else:
-        waterPhase = "ice"
+    mask = [0 for i in range(len(PvapConsts[:,0]))]
+    for i in range(len(PvapConsts[:,0])):
+        if sum(numpy.isin(element = PvapConsts[:,1],test_elements=PvapConsts[i,1])) > 1:
+            structureMask = numpy.isin(element = PvapConsts[:,0],test_elements="I")
+            componentMask = numpy.isin(element = PvapConsts[:,1],test_elements=PvapConsts[i,1])
+            mask[i] = numpy.logical_and(structureMask, componentMask)[i]
+        else:
+            mask[i] = True
         
-    eosResult = PengRobinson(compoundData, moleFractions, temperature, pressure)
-    vaporFugacities = eosResult[2]
-        
-    f_w = waterFugacity(temperature, pressure, waterPhase, vaporFugacities, compounds, compoundData[:,6])
-        
-    f_h = hydrateFugacity(temperature, pressure, PvapConsts, structure, vaporFugacities, compounds, kiharaParameters)
+    localPvapConsts = PvapConsts[mask]
 
-    while abs(f_h/f_w - 1) > errorMargin:
-        pGuess = pGuess*(f_h/f_w)
+    SIEqPressure = abs(scipy.optimize.fsolve(f,pGuess,xtol=errorMargin)[0])
+    try:
         
-        vaporFugacities = PengRobinson(compoundData, moleFractions, temperature, pGuess)[2]
-        f_w = waterFugacity(temperature, pGuess, waterPhase, vaporFugacities, compounds, compoundData[:,6])
-        f_h = hydrateFugacity(temperature, pGuess, PvapConsts, structure, vaporFugacities, compounds, kiharaParameters)
-
-    SIEqPressure = pGuess
-
-    structure = "II"
+        SIEqFrac = hydrateFugacity(temperature, SIEqPressure, localPvapConsts, structure, PengRobinson(compoundData, moleFractions, temperature, pressure)[2], compounds, kiharaParameters, moleFractions)[1]
+    except:
+        SIEqPressure = math.inf
+        SIEqFrac = [[0 for i in range(len(moleFractions))],[0 for i in range(len(moleFractions))]]
+    
     pGuess = pressure
-
-    eosResult = PengRobinson(compoundData, moleFractions, temperature, pressure)
-    vaporFugacities = eosResult[2]
-        
-    f_w = waterFugacity(temperature, pressure, waterPhase, vaporFugacities, compounds, compoundData[:,6])
-        
-    f_h = hydrateFugacity(temperature, pressure, PvapConsts, structure, vaporFugacities, compounds, kiharaParameters)
-
-    while abs(f_h/f_w - 1) > errorMargin:
-        pGuess = pGuess*(f_h/f_w)
-            
-        vaporFugacities = PengRobinson(compoundData, moleFractions, temperature, pGuess)[2]
-        f_w = waterFugacity(temperature, pGuess, waterPhase, vaporFugacities, compounds, compoundData[:,6])
-        f_h = hydrateFugacity(temperature, pGuess, PvapConsts, structure, vaporFugacities, compounds, kiharaParameters)
-
-    SIIEqPressure = pGuess
     
-    return min(SIEqPressure, SIIEqPressure)
+    structure = "II"
+    mask = [0 for i in range(len(PvapConsts[:,0]))]
+    for i in range(len(PvapConsts[:,0])):
+        if sum(numpy.isin(element = PvapConsts[:,1],test_elements=PvapConsts[i,1])) > 1:
+            structureMask = numpy.isin(element = PvapConsts[:,0],test_elements="II")
+            componentMask = numpy.isin(element = PvapConsts[:,1],test_elements=PvapConsts[i,1])
+            mask[i] = numpy.logical_and(structureMask, componentMask)[i]
+        else:
+            mask[i] = True
+        
+    localPvapConsts = PvapConsts[mask]
+        
+    try:
+        SIIEqPressure = abs(scipy.optimize.fsolve(f,[pGuess],xtol=errorMargin)[0])
+        SIIEqFrac = hydrateFugacity(temperature, SIIEqPressure, localPvapConsts, structure, PengRobinson(compoundData, moleFractions, temperature, pressure)[2], compounds, kiharaParameters, moleFractions)[1]
+    except:
+        SIIEqPressure = math.inf
+        SIIEqFrac = [[0 for i in range(len(moleFractions))],[0 for i in range(len(moleFractions))]]
     
+    if SIIEqPressure >= SIEqPressure:
+        eqStructure = "I"
+        EqFrac = SIEqFrac
+    else:
+        eqStructure = "II"
+        EqFrac = SIIEqFrac
+    
+    return min(SIEqPressure, SIIEqPressure), eqStructure, EqFrac
+'''
 #Runtime-----------------------------------------------------------------------
-minTemp = float(input("Lower Temperature (K): ")) #Temp in K
-maxTemp = float(input("Upper Temperature (K): "))
-pressure = float(input("Pressure (MPa): "))*1E6 #Pressure in Pa
+usecsv = input("Import .csv file? (Y/N): ")
+if usecsv == "Y":
+    filename = input("File name: ")
+    guessFile = numpy.genfromtxt(filename, delimiter=',', skip_header=1)
+    T = guessFile[:,0]
+    P = guessFile[:,1]*1E6
+    noPoints = len(T)
+else:
+    minTemp = float(input("Lower Temperature (K): ")) #Temp in K
+    maxTemp = float(input("Upper Temperature (K): "))
+    minGuessPressure = float(input("Minimum Guess Pressure (MPa): "))*1E6 #Pressure in Pa
+    maxGuessPressure = float(input("Maximum Guess Pressure (MPa): "))*1E6 #Pressure in Pa
+    noPoints = int(input("Number of Data Points: "))
+    T = numpy.arange(maxTemp, minTemp-(maxTemp-minTemp)/noPoints, -1*(maxTemp-minTemp)/(noPoints-1))
+    P = numpy.arange(maxGuessPressure, minGuessPressure-(maxGuessPressure-minGuessPressure)/noPoints, -1*(maxGuessPressure-minGuessPressure)/(noPoints-1))
 numberOfCompounds = int(input("No. of Compounds Excluding Water: "))
 compounds = []
 moleFractions = []
@@ -281,12 +355,17 @@ for i in range(numberOfCompounds):
         moleFractions += [float(input("Mole Fraction of Compound " + str(i + 1) + " : "))]
     else:
         moleFractions = [1]
-noPoints = int(input("Number of Data Points: "))
 
-T = numpy.arange(minTemp, maxTemp+(maxTemp-minTemp)/noPoints, (maxTemp-minTemp)/(noPoints-1))
-eqPressure = [0 for i in range(len(T))]
+print("Calculating...")
+startTime = time.time()
+eqPressure = numpy.array([0 for i in range(len(T))],dtype=float)
+eqStructure = [0 for i in range(len(T))]
 for i in range(len(T)):
-    eqPressure[i] = equilibriumPressure(T[i], pressure, compounds, moleFractions)/1E6 #In MPa
+    convergence = equilibriumPressure(T[i], P[i], compounds, moleFractions)
+    eqPressure[i] = convergence[0]/1E6 #In MPa
+    eqStructure[i] = convergence[1]
+    print("Temperature " + str(i + 1) + " convergence point reached with a Structure " + convergence[1] + " hydrate.")
+    print("Occupancy: " + str(convergence[2]))
 
 plt.plot(T, eqPressure, '-ok')
 plt.yscale("log")
@@ -294,3 +373,8 @@ plt.title("Equilibrium Predictions")
 plt.xlabel("Temperature (K)")
 plt.ylabel("Pressure (MPa)")
 plt.show
+
+endTime = time.time()
+
+print("Time to Complete Calculation: " + str(round(endTime-startTime, 3)) + " seconds")
+print("per Data Point: " + str(round((endTime-startTime)/noPoints, 3)) + " seconds")'''
